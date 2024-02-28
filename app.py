dbb18cb85195edf814366a2b4923eb9c00ba81a0
purch1 <ucheprince26@gmail.com>
2024-02-28 23:53:09 +0100

    My first Python code

---
+++ app.py
@@ -0,0 +1,9 @@
+tempreature = 40
+if tempreature > 30:
+    print("It's warm")
+    print("Drink water")
+elif tempreature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
---
+++ app2.py
@@ -0,0 +1,4 @@
+age = 22
+message = "Not eligible" if age >= 18 else "Eligible"
+
+print(message)

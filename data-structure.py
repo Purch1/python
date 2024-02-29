3f0dbf7014c43c829b78b7db8afd1b4b9acb34ae
purch1 <ucheprince26@gmail.com>
2024-02-29 11:28:22 +0100

    Learnt how to find and sort items in a list

--- data-structure.py
+++ data-structure.py
@@ -10,19 +10,29 @@
 
 
 #Loop over list
-numbers = [1, 2, 3, 4, 5, 5, 7]
+# numbers = [1, 2, 3, 4, 5, 5, 7]
 # for index, number in enumerate(numbers):
 #     print(index, number)
 
 letters = ["a", "b", "c"]
-
 #Add
-letters.append("d")
-letters.insert(2, "bc")
+# letters.append("d")
+# letters.insert(2, "bc")
 
 # Remove
-letters.pop(3)
-letters.remove("bc")
-del letters[0:1]
-letters.clear()
-print(letters)
+# letters.pop(3)
+# letters.remove("bc")
+# del letters[0:1]
+# letters.clear()
+# print(letters)
+
+# Finding item in a list
+print(letters.count("d"))
+if "d" in letters:
+    print(letters.index("d"))
+
+# Sorting list
+numbers = [1, 21, 13, 74, 5, 5, 7]
+# numbers.sort(reverse=True)
+print(sorted(numbers, reverse=True))
+print(numbers)

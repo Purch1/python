ee481d26a4a842d597ad90a8b93d2389cc585688
purch1 <ucheprince26@gmail.com>
2024-03-01 13:50:22 +0100

    Learnt how to raise and handle exception the traditional way

--- Exceptions.py
+++ Exceptions.py
@@ -1,10 +1,23 @@
+# try:
+#     with open("app.py") as file:
+#         print("File opened")
+#     age = int(input("Age: "))
+#     xfactor = 10 / age
+# except ValueError:
+#     print("You didn't enter a valid age.")
+# else:
+#     print("No exceptions were throws")
+# print("Excution continues.")
+
+
+# How to raise exceptions
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+    return 10 / age
+
+
 try:
-    with open("app.py") as file:
-        print("File opened")
-    age = int(input("Age: "))
-    xfactor = 10 / age
-except ValueError:
-    print("You didn't enter a valid age.")
-else:
-    print("No exceptions were throws")
-print("Excution continues.")
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)

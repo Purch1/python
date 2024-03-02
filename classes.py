e44c7cb9565fcacb83a89ce206e9e900bee86ef8
purch1 <ucheprince26@gmail.com>
2024-03-02 13:30:39 +0100

    Learnt about inheritance

--- classes.py
+++ classes.py
@@ -1,12 +1,52 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
-    def draw(self):
-        print(f"Point ({self.x}, {self.y})")
+#     def __str__(self):
+#         return f"({self.x}, {self.y})"
+
+#     def draw(self):
+#         print(f"Point ({self.x}, {self.y})")
 
 
-point = Point(1, 2)
-point.draw()
+# point = Point(1, 2)
+# print(str(point))
 
+
+# Making Custom Containers
+# class TagCloud:
+#     def __init__(self):
+#         self.tags = {}
+
+#     def add(self, tag):
+#         self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+
+
+# cloud = TagCloud()
+# cloud.add("Python")
+# cloud.add("python")
+# cloud.add("python")
+# print(cloud.tags)
+
+# Inheritance
+class Animal:
+    def __init__(self):
+        self.age = 1
+        
+    def eat(self):
+        print("eat")
+
+class Mammal(Animal):
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+m = Mammal()
+print(isinstance(m, object))
+o = object()
+o.
